2d80d1556848fd01393789c3afcfed52eea3974a
Beatriz Trombeta <beatriztrombeta@gmail.com>
2025-10-20 11:27:43 -0300

    created login and auth routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthView from '@/views/AuthView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const routes = [
-
+    {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path:'/auth',
+    component: AuthView,
+  },
+  {
+    path: '/login',
+    component: LoginView,
+  },
 ]
 
 const router = createRouter({
