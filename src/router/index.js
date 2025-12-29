de9b131f3c0047197ca22f4725a7776447c4e7c4
beatriztrombeta <beatriztrombeta@gmail.com>
2025-12-29 10:44:27 -0300

    created signup routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AuthView from '@/views/AuthView.vue'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import SignupView from '@/views/signup/SignupView.vue'
 
 const routes = [
-    {
+  {
     path: '/',
     redirect: '/login',
   },
   {
-    path:'/auth',
+    path: '/auth',
     component: AuthView,
-  },
-  {
-    path: '/home',
-    component: HomeView,
   },
   {
     path: '/login',
     component: LoginView,
   },
+  {
+    path: '/signup',
+    component: SignupView,
+  },
+  {
+    path: '/home',
+    component: HomeView,
+  }
 ]
 
 const router = createRouter({
